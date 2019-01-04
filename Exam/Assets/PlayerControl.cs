3510231f9536f567ce3924b36d80b1cd745826d3
riciPrini <35687888+riciPrini@users.noreply.github.com>
2019-01-04 12:34:03 +0100

    First commit and Added some fixes

---
+++ Exam/Assets/PlayerControl.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class PlayerControl : MonoBehaviour {
+    public Camera cam;
+    public Text txt;
+    public NavMeshAgent ag;
+    private Vector3 R390=new Vector3(949,1030,0);
+    private Vector3 R380 = new Vector3(763, 855, 0);
+    private Vector3 R370 = new Vector3(348, 1030, 0);
+    private Vector3 R360 = new Vector3(185, 1006, 0);
+    public void Room390()
+    {
+        Debug.Log(Input.mousePosition);
+        Ray ray = cam.ScreenPointToRay(R390);
+        txt.text = Convert.ToString(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+
+            ag.SetDestination(hit.point);
+
+        }
+
+    }
+    public void Room380(){
+        Debug.Log(Input.mousePosition);
+        Ray ray = cam.ScreenPointToRay(R380);
+        txt.text = Convert.ToString(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+
+            ag.SetDestination(hit.point);
+
+        }
+       
+    }
+    public void Room370()
+    {
+        Debug.Log(Input.mousePosition);
+        Ray ray = cam.ScreenPointToRay(R370);
+        txt.text = Convert.ToString(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+
+            ag.SetDestination(hit.point);
+
+        }
+
+    }
+    public void Room360()
+    {
+        Debug.Log(Input.mousePosition);
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        txt.text = Convert.ToString(R360);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+
+            ag.SetDestination(hit.point);
+
+        }
+
+    }
+    void Update()
+    { 
+       //if (Input.GetMouseButtonDown(0))
+       // {
+       //     Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+       //     Debug.Log(Input.mousePosition);
+       //     RaycastHit hit;
+       //     if (Physics.Raycast(ray, out hit))
+       //     {
+
+       //         ag.SetDestination(hit.point);
+
+       //     }
+
+       // }
+    }
+    }
---
+++ Exam/Assets/drawline.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class drawline : MonoBehaviour {
+    public NavMeshAgent ag;
+    public LineRenderer line;
+    // Use this for initialization
+    void Start () {
+        line = GetComponent<LineRenderer>();
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (ag.hasPath)
+        {
+            line.positionCount=ag.path.corners.Length;
+            line.SetPositions(ag.path.corners);
+            line.enabled = true;
+        }else
+        {
+            line.enabled = false;
+
+        }
+	}
+}
